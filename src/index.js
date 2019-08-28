cd3444a41a8b620323cd28af85af464950a33ce2
Syed Nazmus Shakib <sakibsyed01@gmail.com>
2019-08-28 06:52:23 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorker from './serviceWorker';
+import Router from './components/Router';
+
+ReactDOM.render(<Router />, document.getElementById('root'));
+serviceWorker.unregister();
